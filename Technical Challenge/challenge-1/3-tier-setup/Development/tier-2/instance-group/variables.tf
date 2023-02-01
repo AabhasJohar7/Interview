a3f10b7a424651b80e123ea370b4390555295e84
AabhasJohar7 <123113739+AabhasJohar7@users.noreply.github.com>
2023-02-01 16:58:59 +0530

    Add files via upload

---
+++ Technical Challenge/challenge-1/3-tier-setup/Development/tier-2/instance-group/variables.tf
@@ -0,0 +1,15 @@
+
+variable "project_id" {
+  description = "Google Project ID."
+  type        = string
+}
+
+variable "region" {
+ description = "Google Cloud region"
+ type        = string
+}
+
+variable "imagename" {
+  description = "serviceemail"
+  type        = string
+}
